d9d5a9ba9a6c0359d4de299982572d3ddd75ffa0
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2024-11-19 08:58:36 +0700

    feat: add font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Paytone+One&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="antialiased bg-background text-one">
         <Main />
         <NextScript />
       </body>
